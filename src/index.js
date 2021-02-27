91b8f2f51d615867519cbac6411c5e62f4ad3a38
SpiriT-L <m.leon-id@yandex.ru>
2021-02-27 18:26:37 +0300

    fix: adding a solution to a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        let element = checkIt(bracketsConfig, str[i], false);
+        if (element) {
+            if (element[0] === element[1]) {
+                if (arr.length) {
+                    if (arr[arr.length - 1] === str[i]) arr.pop();
+                    else arr.push(element[0]);
+                } else arr.push(element[0]);
+            } else arr.push(element[1]);
+        } else {
+            element = checkIt(bracketsConfig, str[i], true);
+            if (element) {
+                if (!arr.length) return false;
+                if (str[i] !== arr.pop()) return false;
+            } else return false;
+        }
+    }
+
+    if (arr.length) return false;
+    else return true;
+};
+
+function checkIt(arr, item, isCloseBrack) {
+    let Pos = Number(isCloseBrack);
+    for (let i = 0; i < arr.length; i++)
+        if (arr[i][Pos] === item) {
+            return arr[i];
+        }
+    return false;
 }
